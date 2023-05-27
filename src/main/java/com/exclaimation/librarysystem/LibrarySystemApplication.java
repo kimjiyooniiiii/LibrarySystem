16b78bb3719fc7dc8db0dfcf66179a9add8c2f9b
oeanb12 <oeanb12@gmail.com>
2023-05-28 02:56:32 +0900

    main page

---
+++ src/main/java/com/exclaimation/librarysystem/LibrarySystemApplication.java
@@ -0,0 +1,14 @@
+package com.exclaimation.librarysystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+
+@SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
+public class LibrarySystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LibrarySystemApplication.class, args);
+    }
+
+}
