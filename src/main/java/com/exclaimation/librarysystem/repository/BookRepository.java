f22c4980db48b5c385b53f876ab2dd8443692101
star-moon-cloud-k <kspecial44@gmail.com>
2023-06-10 14:55:28 +0900

    FIX : 검색 repository 수정

--- src/main/java/com/exclaimation/librarysystem/repository/BookRepository.java
+++ src/main/java/com/exclaimation/librarysystem/repository/BookRepository.java
@@ -5,10 +5,11 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 
 public interface BookRepository extends JpaRepository<Book, Long> {
 
     @Query("select b from Book b where b.title like concat('%' , :title , '%') or b.content like concat('%', :content, '%')")
-    Page<Book> findByKeyword(String title, String content, Pageable page);
+    Page<Book> findByKeyword(@Param("title") String title,@Param("content") String content, Pageable page);
 }
